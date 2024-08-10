fe5664fa7a571280eef82d49297b5f09a30ba95b
0xDon <22967202+thedonmon@users.noreply.github.com>
2024-08-10 12:20:13 -0400

    update utils

---
+++ src/components/transfer/TransferContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { FetchedTokenInfo } from '@/lib/types';
+
+interface TransferItem {
+    type: 'SOL' | 'SPL' | 'NFT';
+    amount: string;
+    token?: FetchedTokenInfo;
+}
+
+interface TransferContextType {
+    transferItems: TransferItem[];
+    addTransferItem: (item: TransferItem) => void;
+    removeTransferItem: (index: number) => void;
+    clearTransferItems: () => void;
+}
+
+const TransferContext = createContext<TransferContextType | undefined>(undefined);
+
+export const TransferProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [transferItems, setTransferItems] = useState<TransferItem[]>([]);
+
+    const addTransferItem = (item: TransferItem) => {
+        setTransferItems(prev => [...prev, item]);
+    };
+
+    const removeTransferItem = (index: number) => {
+        setTransferItems(prev => prev.filter((_, i) => i !== index));
+    };
+
+    const clearTransferItems = () => {
+        setTransferItems([]);
+    };
+
+    return (
+        <TransferContext.Provider value={{ transferItems, addTransferItem, removeTransferItem, clearTransferItems }}>
+            {children}
+        </TransferContext.Provider>
+    );
+};
+
+export const useTransfer = () => {
+    const context = useContext(TransferContext);
+    if (context === undefined) {
+        throw new Error('useTransfer must be used within a TransferProvider');
+    }
+    return context;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,7 +12,9 @@ import {
   Connection,
   ParsedAccountData,
   PublicKey,
+  SystemProgram,
   Transaction,
+  TransactionInstruction,
 } from "@solana/web3.js";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -73,74 +75,17 @@ export async function fetchTokenInfo(
         decimals: account.account.data.parsed.info.tokenAmount.decimals,
         metadata: tokenMetadata
           ? {
-              name: tokenMetadata?.name,
-              symbol: tokenMetadata?.symbol,
-              json: tokenMetadata?.uri,
-              image: imageUri,
-              attributes,
-            }
+            name: tokenMetadata?.name,
+            symbol: tokenMetadata?.symbol,
+            json: tokenMetadata?.uri,
+            image: imageUri,
+            attributes,
+          }
           : null,
       };
     })
   );
 }
-
-// export async function fetchTokenInfoFromAddresses(
-//   connection: Connection,
-//   addresses: PublicKey[],
-//   programId: PublicKey,
-//   fetchTokenMetadata: boolean
-// ): Promise<FetchedTokenInfo[]> {
-//   return Promise.all(
-//     addresses.map(async (account) => {
-//       let tokenMetadata: any = null;
-//       let imageUri: string | null = null;
-//       let attributes: Attribute[] = [];
-//       if (fetchTokenMetadata) {
-//         try {
-//           tokenMetadata = await getTokenMetadata(
-//             connection,
-//             account,
-//             "confirmed",
-//             programId
-//           );
-//           if (tokenMetadata && tokenMetadata.uri) {
-//             try {
-//               const metadataResponse = await fetch(tokenMetadata.uri);
-//               const metadata = await metadataResponse.json();
-//               imageUri = metadata.image;
-//               attributes = metadata.attributes;
-//             } catch (error) {
-//               console.error(
-//                 "Error fetching or parsing metadata:",
-//                 error,
-//                 tokenMetadata.uri
-//               );
-//             }
-//           }
-//         } catch (err) {
-//           console.log("Error fetching token metadata:", err);
-//         }
-//       }
-//       return {
-//         owner: account.account.owner.toBase58(),
-//         mint: account.account.data.parsed.info.mint,
-//         amount: account.account.data.parsed.info.tokenAmount.uiAmount,
-//         tokenAccount: account.pubkey.toBase58(),
-//         decimals: account.account.data.parsed.info.tokenAmount.decimals,
-//         metadata: tokenMetadata
-//           ? {
-//             name: tokenMetadata?.name,
-//             symbol: tokenMetadata?.symbol,
-//             json: tokenMetadata?.uri,
-//             image: imageUri,
-//             attributes
-//           }
-//           : null,
-//       };
-//     })
-//   );
-// }
 
 export async function createTransferTransaction(
   connection: Connection,
@@ -152,9 +97,45 @@ export async function createTransferTransaction(
 ): Promise<Transaction> {
   const transaction = new Transaction();
 
+  const instructions = await createTokenTransferInstructions(
+    connection,
+    publicKey,
+    new PublicKey(destinationAddress),
+    token,
+    amount,
+    programId
+  );
+
+  transaction.add(...instructions);
+
+  return transaction;
+}
+
+export const createSolTransferInstruction = (
+  fromPubkey: PublicKey,
+  toPubkey: PublicKey,
+  amount: number
+): TransactionInstruction => {
+  return SystemProgram.transfer({
+    fromPubkey,
+    toPubkey,
+    lamports: amount,
+  });
+};
+
+export const createTokenTransferInstructions = async (
+  connection: Connection,
+  fromPubkey: PublicKey,
+  toPubkey: PublicKey,
+  token: FetchedTokenInfo,
+  amount: number,
+  programId: typeof TOKEN_PROGRAM_ID | typeof TOKEN_2022_PROGRAM_ID
+): Promise<TransactionInstruction[]> => {
+  const instructions: TransactionInstruction[] = [];
+
   const destinationTokenAccount = getAssociatedTokenAddressSync(
     new PublicKey(token.mint),
-    new PublicKey(destinationAddress),
+    toPubkey,
     true,
     programId
   );
@@ -162,39 +143,40 @@ export async function createTransferTransaction(
   // Check if the destination token account exists
   const accountInfo = await connection.getAccountInfo(destinationTokenAccount);
   if (!accountInfo || !accountInfo.data || accountInfo.data.length === 0) {
-    transaction.add(
+    instructions.push(
       createAssociatedTokenAccountInstruction(
-        publicKey,
+        fromPubkey,
         destinationTokenAccount,
-        new PublicKey(destinationAddress),
+        toPubkey,
         new PublicKey(token.mint),
         programId
       )
     );
   }
 
-  const transferInstruction = createTransferInstruction(
-    new PublicKey(token.tokenAccount),
-    destinationTokenAccount,
-    publicKey,
-    amount * Math.pow(10, token.decimals),
-    [],
-    programId
+  instructions.push(
+    createTransferInstruction(
+      new PublicKey(token.tokenAccount),
+      destinationTokenAccount,
+      fromPubkey,
+      amount * Math.pow(10, token.decimals),
+      [],
+      programId
+    )
   );
-
-  transaction.add(transferInstruction);
 
   // Close account if all tokens are being transferred
   if (amount === token.amount) {
-    const tokenCloseInstruction = createCloseAccountInstruction(
-      new PublicKey(token.tokenAccount),
-      publicKey,
-      publicKey,
-      [],
-      programId
+    instructions.push(
+      createCloseAccountInstruction(
+        new PublicKey(token.tokenAccount),
+        fromPubkey,
+        fromPubkey,
+        [],
+        programId
+      )
     );
-    transaction.add(tokenCloseInstruction);
   }
 
-  return transaction;
-}
+  return instructions;
+};
