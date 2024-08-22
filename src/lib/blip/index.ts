1701d422fd5f675629db90fbcb08cfdfd0d99aac
rob shaw <rob@statik.dev>
2024-08-22 16:03:59 -0400

    feat: use nifty asset group for mint

--- src/lib/blip/index.ts
+++ src/lib/blip/index.ts
@@ -1,6 +1,7 @@
 import { transferSol } from "@metaplex-foundation/mpl-toolbox";
 import {
   createNoopSigner,
+  createSignerFromKeypair,
   generateSigner,
   publicKey,
   signerIdentity,
@@ -9,9 +10,11 @@ import {
 } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { base64 } from "@metaplex-foundation/umi/serializers";
-import { creators, metadata, mint, niftyAsset } from "@nifty-oss/asset";
+import { creators, group, metadata, mint, niftyAsset } from "@nifty-oss/asset";
 
 const BLIP_FEE__WALLET_ADDRESS = "6juCmFHoPnJTzhjJfcjFhCXeptCE89vp9dHP91EUaxR8";
+const BLIP_GROUP__ACCOUNT_ADDRESS =
+  "b1ipWX4C4xYRYvNg2YRPM6EUofH9NqU8PwUtTK5VCrx";
 
 export async function generateBlipTransaction(
   from: string,
@@ -25,11 +28,18 @@ export async function generateBlipTransaction(
     .use(niftyAsset())
     .use(signerIdentity(fromNoopSigner, true));
 
+  const groupAccountUint8 = new Uint8Array(
+    JSON.parse(process.env.BLIP_NIFTY_GROUP_AUTHORITY_SECRET_KEY ?? "[]")
+  );
+  const groupAccountKeypair =
+    umi.eddsa.createKeypairFromSecretKey(groupAccountUint8);
+  const groupAccountSigner = createSignerFromKeypair(umi, groupAccountKeypair);
+
   const assetSigner = generateSigner(umi);
   const mintBuilderGroup = mint(umi, {
     asset: assetSigner,
     owner: publicKey(to),
-    authority: publicKey(to),
+    authority: groupAccountSigner.publicKey,
     mutable: false,
     payer: fromNoopSigner,
     standard: 0,
@@ -51,12 +61,20 @@ export async function generateBlipTransaction(
     amount: sol(0.001),
   });
 
+  const groupIx = group(umi, {
+    authority: groupAccountSigner,
+    group: publicKey(BLIP_GROUP__ACCOUNT_ADDRESS),
+    asset: assetSigner.publicKey,
+  });
+
   const txn = await transactionBuilder()
     .add(transferSolIx)
     .add(mintBuilderGroup.merge())
+    .add(groupIx)
     .buildWithLatestBlockhash(umi);
 
   const nftSignedTxn = await assetSigner.signTransaction(txn);
-  const serializedTxn = umi.transactions.serialize(nftSignedTxn);
+  const groupSignedTxn = await groupAccountSigner.signTransaction(nftSignedTxn);
+  const serializedTxn = umi.transactions.serialize(groupSignedTxn);
   return base64.deserialize(serializedTxn)[0];
 }
