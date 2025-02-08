c44f4cdbfe84c1dd47524485225f7eaec310a85b
rob shaw <rob@statik.dev>
2025-02-07 23:23:42 -0500

    chore: 3 per line

--- src/app/blip/page.tsx
+++ src/app/blip/page.tsx
@@ -342,7 +342,7 @@ export default function MessagePage() {
                   <p className="pb-2 text-center font-medium">
                     Select Template
                   </p>
-                  <div className="grid grid-cols-3 gap-4 sm:grid-cols-4">
+                  <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
                     {isLoadingTemplates
                       ? Array(3)
                           .fill(0)
