de99a90bc09aba798af76df1ade81de668e039e0
rob shaw <rob@statik.dev>
2025-02-12 00:46:04 -0500

    feat: valentines inspired unfurl for /blip

--- src/app/blip/layout.tsx
+++ src/app/blip/layout.tsx
@@ -1,31 +1,36 @@
 import { Metadata } from "next";
+
 import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
-    title: 'Send Messages on Eclipse | Validators.wtf',
-    description: 'Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.',
-    keywords: 'Eclipse, blockchain messaging, on-chain messages, Blip, validators.wtf, blockchain communication',
-    openGraph: {
-        title: 'Send Messages on Eclipse | Validators.wtf',
-        description: 'Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.',
-        url: 'https://validators.wtf/blip',
-        siteName: 'Validators.wtf',
-        type: 'website',
-        images: [siteConfig.ogImage],
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Send Messages on Eclipse | Validators.wtf',
-        description: 'Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.',
-        images: [siteConfig.ogImage],
-        creator: "@Validators_",
-    }
+  title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+  description:
+    "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
+  keywords:
+    "Eclipse, blockchain messaging, Valentine messages, love letters, Blip, validators.wtf, blockchain romance, crypto valentine",
+  openGraph: {
+    title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+    description:
+      "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
+    url: "https://validators.wtf/blip",
+    siteName: "Validators.wtf",
+    type: "website",
+    images: ["https://www.validators.wtf/logo/logovalentines1.svg"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+    description:
+      "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
+    images: ["https://www.validators.wtf/logo/logovalentines1.svg"],
+    creator: "@Validators_",
+  },
 };
 
 export default function BlipLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return <>{children}</>;
-} 
+  return <>{children}</>;
+}
