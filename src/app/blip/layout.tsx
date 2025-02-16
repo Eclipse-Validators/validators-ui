e783aec6888af957bc2d454561f806b888cb8a65
rob shaw <rob@statik.dev>
2025-02-16 00:21:36 -0500

    revert: vday blip page unfurl
    This reverts commit de99a90bc09aba798af76df1ade81de668e039e0.

--- src/app/blip/layout.tsx
+++ src/app/blip/layout.tsx
@@ -3,26 +3,26 @@ import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
-  title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+  title: "Send Messages on Eclipse | Validators.wtf",
   description:
-    "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
+    "Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.",
   keywords:
-    "Eclipse, blockchain messaging, Valentine messages, love letters, Blip, validators.wtf, blockchain romance, crypto valentine",
+    "Eclipse, blockchain messaging, on-chain messages, Blip, validators.wtf, blockchain communication",
   openGraph: {
-    title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+    title: "Send Messages on Eclipse | Validators.wtf",
     description:
-      "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
+      "Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.",
     url: "https://validators.wtf/blip",
     siteName: "Validators.wtf",
     type: "website",
-    images: ["https://www.validators.wtf/blip/logovalentines1.png"],
+    images: [siteConfig.ogImage],
   },
   twitter: {
     card: "summary_large_image",
-    title: "💝 Send Valentine Messages on Eclipse | Validators.wtf",
+    title: "Send Messages on Eclipse | Validators.wtf",
     description:
-      "Share your love on the blockchain! Send heartfelt Valentine messages that last forever on Eclipse. Connect your wallet to send Blips - where romance meets web3.",
-    images: ["https://www.validators.wtf/blip/logovalentines1.png"],
+      "Send on-chain messages to any wallet on Eclipse. Connect your wallet to send Blips - personalized messages stored permanently on the blockchain.",
+    images: [siteConfig.ogImage],
     creator: "@Validators_",
   },
 };
